73393ef984654e5eeccebac8484282d75174678c
Claudio Kressibucher <ckressibucher@graphicworks.ch>
2016-05-20 14:04:26 +0200

    add some tests; add doMap function

---
+++ CsvFunctionsTest.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace CSV\Test;
+
+use CSV;
+
+use RuntimeException;
+use PHPUnit_Framework_TestCase;
+
+class CsvFunctionsTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function its_toAssoc_maps_header_row_to_all_following_rows()
+    {
+        $input = [
+            ['a', 'b', 'c'],
+            [1, 2, 3],
+            [11, 22, 33],
+        ];
+        $expected = [
+            ['a' => 1, 'b' => 2, 'c' => 3],
+            ['a' => 11, 'b' => 22, 'c' => 33],
+        ];
+        $output = CSV\toAssoc($this->toIterable($input));
+        $this->assertEquals($expected, iterator_to_array($output));
+    }
+
+    /**
+     * @test
+     */
+    public function its_toAssoc_throws_error_if_not_same_length()
+    {
+        $input = [
+            ['a', 'b', 'c'],
+            [1, 2], // only two cols
+        ];
+        $this->expectException(RuntimeException::class);
+        $out = CSV\toAssoc($this->toIterable($input));
+        iterator_to_array($out); //  execute generator
+    }
+
+    /**
+     * @test
+     */
+    public function its_doSelect_selects_columns_by_key()
+    {
+        $input = [['a' => 1, 'b' => 20, 'c' => 2]];
+        $expected = [['a' => 1, 'b' => 20]];
+        $out = CSV\doSelect($this->toIterable($input), ['a', 'b']);
+        $this->assertEquals($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_doFilter_filters_data_rows()
+    {
+        $input = [
+            ['condition' => true, 'val' => 2],
+            ['condition' => false, 'val' => 3],
+        ];
+        $expected = [
+            ['condition' => true, 'val' => 2],
+        ];
+        $filter = function ($row) {
+            return $row['condition'];
+        };
+        $out = CSV\doFilter($this->toIterable($input), $filter);
+        $this->assertEquals($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_doMap_maps_rows()
+    {
+        $input = [
+            'abc',
+            'yyy',
+        ];
+        $expected = [
+            'cba',
+            'yyy'
+        ];
+        $out = CSV\doMap($this->toIterable($input), 'strrev');
+        $this->assertEquals($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_doMap_returns_empty_generator_on_empty_input()
+    {
+        $input = new \ArrayObject();
+        $out = CSV\doMap($input, 'strrev');
+        $this->assertInstanceOf(\Generator::class, $out);
+        $this->assertSame([], iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_buildFilter_creates_filterable_closure_yielding_a_generator()
+    {
+        $input = [['a' => 1]];
+        $expected = [];
+
+        $filter = function ($row) {
+            return false; // dummy
+        };
+        // this should now be a callable which transforms a Traversable to a Generator
+        $filterCallback = CSV\buildFilter($filter);
+        $this->assertTrue(is_callable($filterCallback));
+
+        $out = $filterCallback($this->toIterable($input));
+        $this->assertInstanceOf(\Generator::class, $out);
+        $this->assertSame($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_buildSelect_creates_a_select_closure_yielding_a_generator()
+    {
+        $input = [['a' => 1, 'b' => 2]];
+        $fields = ['a'];
+        $expected = [['a' => 1]];
+
+        $selectable = CSV\buildSelect($fields);
+        $this->assertTrue(is_callable($selectable));
+
+        $out = $selectable($this->toIterable($input));
+        $this->assertInstanceOf(\Generator::class, $out);
+        $this->assertEquals($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_combine_stages_combines_processor_functions()
+    {
+        $input = [
+            ['a' => 11, 'b' => 22],
+            []
+        ];
+        $expected = [ // filter and convert to string
+            ['a' => '11', 'b' => '22']
+        ];
+        $filter = function (\Traversable $data) {
+            foreach ($data as $row) {
+                if (! empty($row)) {
+                    yield $row;
+                }
+            }
+        };
+        $toString = function (\Traversable $data) {
+            foreach ($data as $row) {
+                yield array_map('strval', $row);
+            }
+        };
+        $pipeline = CSV\combineStages($filter, $toString);
+        $this->assertTrue(is_callable($pipeline));
+
+        $out = $pipeline($this->toIterable($input));
+        $this->assertInstanceOf(\Generator::class, $out);
+        $this->assertEquals($expected, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_combine_stages_does_not_alter_input_with_identity_processor()
+    {
+        $input = [
+            ['a' => 1],
+        ];
+        // this does nothing else than "converting" the Traversable to a Generator
+        $idProc = function (\Traversable $data) {
+            foreach ($data as $d) {
+                yield $d;
+            }
+        };
+        $pipeline = CSV\combineStages($idProc);
+        $out = $pipeline($this->toIterable($input));
+        $this->assertEquals($input, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_combine_stages_does_not_alter_input_without_arguments()
+    {
+        $input = [
+            ['a' => 1],
+        ];
+        $pipeline = CSV\combineStages();
+        $out = $pipeline($this->toIterable($input));
+        $this->assertInstanceOf(\Generator::class, $out);
+        $this->assertEquals($input, iterator_to_array($out));
+    }
+
+    /**
+     * @test
+     */
+    public function its_doCount_counts_traversable()
+    {
+        $input = $this->toIterable(['a', 'b']);
+        $this->assertSame(2, CSV\doCount($input));
+    }
+
+    /**
+     * @test
+     */
+    public function its_toPrint_prints_to_stdout()
+    {
+        ob_start();
+        $input = $this->toIterable(['some string']);
+        CSV\doPrint($input);
+        $output = ob_get_clean();
+        $this->assertContains('some string', $output);
+    }
+
+    private function toIterable(array $arr)
+    {
+        return new \ArrayObject($arr);
+    }
+}
---
+++ csv_functions.php
@@ -0,0 +1,284 @@
+<?php
+
+namespace CSV;
+
+use Generator;
+use Traversable;
+
+// =========================================================================
+// == Data Sources                                                  ========
+// =========================================================================
+
+// Note: additionally to using this data sources, you can also directly
+// provide datasets as any kind of `Traversable` data structure
+// to most of the processing functions below.
+
+/**
+ * Reads a file line by line and parses it to CSV
+ *
+ * @param string $path      Filepath
+ * @param string $delimiter CSV Delimiter used for parsing
+ * @param string $enclosure CSV Enclosure used for parsing
+ * @return Generator        Generator or arrays (one arr per line)
+ */
+function readFromFile(
+    string $path,
+    string $delimiter = ',',
+    string $enclosure = '"'
+): Generator {
+    if (!file_exists($path)) {
+        throw new \RuntimeException('file ' . $path . ' does not exist');
+    }
+    $fh = fopen($path, 'rb');
+    foreach (readFromResource($fh, $delimiter, $enclosure) as $resultRow) {
+        yield $resultRow;
+    }
+    fclose($fh);
+}
+
+/**
+ * Reads CSV rows line by line from the given resource
+ *
+ * @param resource $fh      Resource to read from (Will not be closed by this function!)
+ * @param string $delimiter CSV Delimiter used for parsing
+ * @param string $enclosure CSV Enclosure used for parsing
+ * @return Generator        Generator or arrays (one arr per line)
+ */
+function readFromResource(
+    resource $fh,
+    string $delimiter = ',',
+    string $enclosure = '"'
+): Generator {
+    while (false !== ($line = fgetcsv($fh, null, $delimiter, $enclosure))) {
+        if (null === $line) {
+            throw new \RuntimeException('probably invalid file handle');
+        }
+        if (1 === count($line) && null === $line[0]) {
+            continue;
+        }
+        yield $line;
+    }
+}
+
+// =========================================================================
+// == Data Processing functions                                     ========
+// =========================================================================
+
+/**
+ * Maps every row to an assoc array, where the values
+ * of the first row as the keys.
+ *
+ * @param Traversable $data
+ * @return Generator
+ */
+function toAssoc(Traversable $data): Generator
+{
+    $keys = null;
+    foreach ($data as $row) {
+        if (null === $keys) {
+            // first row
+            $keys = $row;
+            continue;
+        }
+        if (count($row) !== count($keys)) {
+            $msg = 'Error combining header keys with row. Probably not the same length';
+            throw new \RuntimeException($msg);
+        }
+        $assoc = array_combine($keys, $row);
+        if (!is_array($assoc)) {
+            throw new \RuntimeException('something went wrong');
+        }
+        yield $assoc;
+    }
+}
+
+/**
+ * Selects fields from assoc dataset
+ *
+ * @see toAssoc to generate an assoc dataset
+ *
+ * @param Traversable $assocData
+ * @param array $fields
+ * @return Generator
+ */
+function doSelect(Traversable $assocData, array $fields): Generator
+{
+    foreach ($assocData as $row) {
+        $newRow = [];
+        foreach ($row as $k => $v) {
+            if (in_array($k, $fields)) {
+                $newRow[$k] = $v;
+            }
+        }
+        yield $newRow;
+    }
+}
+
+/**
+ * @param Traversable $data
+ * @param callable $filter Gets an row (of the $data) as input and is
+ *                         expected to return a bool: true if row should
+ *                         be kept, false if it should not be yielded for
+ *                         the new generator
+ * @return Generator
+ */
+function doFilter(Traversable $data, callable $filter): Generator
+{
+    foreach ($data as $row) {
+        if (call_user_func($filter, $row)) {
+            yield $row;
+        }
+    }
+}
+
+/**
+ * Applies the mapper function to each data record
+ *
+ * @param Traversable $data
+ * @param callable $mapper
+ * @return Generator
+ */
+function doMap(Traversable $data, callable $mapper): Generator
+{
+    foreach ($data as $row) {
+        yield $mapper($row);
+    }
+}
+
+// =========================================================================
+// === Helpers (builders, combinators)
+// =========================================================================
+
+/**
+ * Builds a filter using `doFilter`
+ *
+ * @see doFilter
+ *
+ * This can be useful to build compositions (pipes, middlewares, ...)
+ * of functions.
+ *
+ * @param callable $filter
+ * @return \Closure
+ */
+function buildFilter(callable $filter): \Closure
+{
+    return function (Traversable $data) use ($filter) {
+        return doFilter($data, $filter);
+    };
+}
+
+/**
+ * Builds a select function
+ *
+ * @param array $fields
+ * @return \Closure
+ */
+function buildSelect(array $fields): \Closure
+{
+    return function (Traversable $data) use ($fields) {
+        return doSelect($data, $fields);
+    };
+}
+
+/**
+ * Lets you combine different "stages" (functions) to
+ * a pipeline of tasks which are executed sequentially
+ * to process a given Traversable input set.
+ *
+ * All is done lazily. The output itself is a function
+ * that maps a Generator to a new Generator, so it could
+ * be used again as a stage for other combinations.
+ *
+ * @param \callable[] ...$stages Each stage is a callable function which maps a
+ *                               Input Traversable to an output Generator
+ * @return \Closure A closure which maps a Traversable input to a Generator
+ */
+function combineStages(callable ...$stages): \Closure
+{
+    return function (Traversable $in) use ($stages) {
+        $data = $in;
+        if (empty($stages) && !$data instanceof Generator) {
+            // convert to generator (according to specs)
+            $idProc = function (\Traversable $data) {
+                foreach ($data as $d) {
+                    yield $d;
+                }
+            };
+            array_unshift($stages, $idProc);
+        }
+        foreach ($stages as $stage) {
+            $data = call_user_func($stage, $data);
+        }
+        return $data;
+    };
+}
+
+// =========================================================================
+// === Data Sinks
+// =========================================================================
+
+/**
+ * Writes the data given by the generator to the csv file
+ *
+ * @param Traversable $data
+ * @param string $path
+ * @param string $delimiter
+ * @param string $enclosure
+ * @param bool $overwrite
+ */
+function writeToFile(
+    Traversable $data,
+    string $path,
+    string $delimiter = ',',
+    string $enclosure = '"',
+    $overwrite = true
+) {
+    if (file_exists($path)) {
+        $ok = false;
+        if ($overwrite) {
+            $ok = unlink($path);
+        }
+        if (!$ok) {
+            throw new \RuntimeException('there already is a file at ' . $path);
+        }
+    }
+    $ok = touch($path);
+    if (!$ok) {
+        throw new \RuntimeException('Can not write to ' . $path);
+    }
+    $fh = fopen($path, 'wb');
+    foreach ($data as $row) {
+        $ok = fputcsv($fh, $row, $delimiter, $enclosure);
+        if (false === $ok) {
+            throw new \RuntimeException('error when writing file ' . $path);
+        }
+    }
+    fclose($fh);
+}
+
+/**
+ * Traverses the data to count it
+ *
+ * @param Traversable $data
+ * @return int
+ */
+function doCount(Traversable $data): int
+{
+    $cnt = 0;
+    foreach ($data as $d) {
+        $cnt++;
+    }
+    return $cnt;
+}
+
+/**
+ * Prints each row
+ *
+ * @param Traversable $data
+ */
+function doPrint(Traversable $data)
+{
+    foreach ($data as $d) {
+        print_r($d);
+    }
+}
